e6d65d6239b4901dddacda8e0cb745fd1c67b049
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-09-28 13:49:42 -0600

    a2 test

---
+++ web/assignments.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>313 Assignments</title>
+  <meta name="Description" content="313 assignments">
+
+</head>
+<?php include 'w2_homepage/header.php'?>
+<body>
+  <h1 style="font-family:courier;">Assignments Coming Soon</h1>
+</body>
+</html>
