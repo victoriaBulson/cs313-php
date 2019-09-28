97ed41f83db23784d8fb98a67eb70b361b45e78c
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-09-27 22:43:55 -0600

    a2 test

--- web/w2_homepage/home.php
+++ web/w2_homepage/home.php
@@ -1,5 +1,3 @@
 <?php
-$image = exif_thumbnail('../pics/wedding1.JPG', $width, $height, $type);
-
-echo "<img  width='$width' height='$height' src='data:image/gif;base64,".base64_encode($image)."'>";
+echo "<img src='../pics/wedding1.JPG' alt='photo of me' />";
 ?>
