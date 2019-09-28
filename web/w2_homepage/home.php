bfcf43b264791542980646208e491ee9f6a61739
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-09-28 12:57:01 -0600

    a2 test

--- web/w2_homepage/home.php
+++ web/w2_homepage/home.php
@@ -1,3 +1,44 @@
-<?php
-echo "<p>Copyright &copy; 1999-" . date("Y") . " W3Schools.com</p>";
-?>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Victoria Mears</title>
+  <meta name="Description" content="Home Page">
+  <meta name="Victoria Mears" content="Home Page">
+  
+  <link rel="stylesheet" type="text/css" href="home.css">
+
+</head>
+
+<header>
+  <h1>Victoria Bulson Mears</h1>
+</header>
+<body>
+  <div id="quote" class="orange">
+    <h3>"We Need Women Who Know How To Make Important Things Happen By Their Faith"</h3>
+    <h5> -Russel M. Nelson </h5>
+  </div>
+  <div id="academics" class="blue">
+     <h3>Academics</h3>
+     <ul>
+        <li>Senior Project</li>
+        <li>Software Engineering 1</li>
+        <li>Project Management</li>
+        <li><a href="../asssignments.html">Web Engineering</a></li>
+     </ul>
+  </div>
+  <div id="family" class="blue">
+    <h3>Family</h3>
+<?php include "home.php";?>
+  </div>
+  <div id="talent" class="orange">
+    <h3 id="talentHead">Talents</h3>
+    <video width="310" height="230" controls>
+      <source src="../pics/redFlags.MOV" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
+  </div>
+
+</body>
+
+</html>
