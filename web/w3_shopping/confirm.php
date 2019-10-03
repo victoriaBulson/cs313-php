57b0f5f524b793afd8e60c658ab0ad020b33535f
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-03 16:35:28 -0600

    start for shopping page.

---
+++ web/w3_shopping/confirm.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Hello World</title>
+  <meta name="Description" content="Hello WOrld">
+  <meta name="Victoria Mears" content="Hello World">
+
+</head>
+
+<body>
+  <h1 style="color:red; fontf-family:courier;">Hello World</h1>
+</body>
+</html>
---
+++ web/w3_shopping/header.php
@@ -0,0 +1,4 @@
+<header>
+    <h1>Miracle Max Apothecary</h1>
+    <img src="pics/handcart.png" alt="Shopping Cart">
+</header>
