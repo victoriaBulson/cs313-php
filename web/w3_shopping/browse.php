9cf92280f69b7b08b9415ce7ed604ec54403758c
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-04 15:56:43 -0600

    edit grid

--- web/w3_shopping/browse.php
+++ web/w3_shopping/browse.php
@@ -8,10 +8,6 @@
 
     <link rel="stylesheet" type="text/css" href="shopping.css">
 </head>
-
-<header>
-    Miracle Max Apothecary <img src="pics/handcart.png" alt="Shopping Cart" style="width:100px; height:100px; float:right;">
-</header>
     
 <body id="browse_page" >
     <div id="header">
