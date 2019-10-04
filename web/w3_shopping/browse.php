24788c74409bfd96606a3e9007a6744320293fba
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-04 15:55:55 -0600

    edit grid

--- web/w3_shopping/browse.php
+++ web/w3_shopping/browse.php
@@ -14,6 +14,9 @@
 </header>
     
 <body id="browse_page" >
+    <div id="header">
+    Miracle Max Apothecary <img src="pics/handcart.png" alt="Shopping Cart" style="width:100px; height:100px; float:right;">
+    </div>
     <div class="browse_row" style="background-color:red;">
         <div class="item_1">
             <img src="pics/love_potion.jpg" alt="Love Potion Image" class="browse_img">
