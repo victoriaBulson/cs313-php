a56e7a32d780e1b3185f2c9a3e75d731b252dff1
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-05 14:16:26 -0600

    adding add to cart button functionality

--- web/w3_shopping/functions.php
+++ web/w3_shopping/functions.php
@@ -3,9 +3,10 @@
     if($_GET['action'] == 'add_to_cart') {addToCart();}
 
     function addToCart(){
+        echo $_SESSION['cart'][1];
         array_push($_SESSION['cart'],$_GET[item]);
         for($i = 0 ; $i < count($_SESSION['cart']) ; $i++) {
-            echo "hello loop!";
-        }
+     echo '<td>'.$_SESSION['cart'][$i].'</td>';
+     }
     }
 ?>
