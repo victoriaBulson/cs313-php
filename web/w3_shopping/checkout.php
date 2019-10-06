6cad02a76c76a5ec4a712a972bd57aa3959c486e
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-05 19:02:15 -0600

    add address form

--- web/w3_shopping/checkout.php
+++ web/w3_shopping/checkout.php
@@ -1,14 +1,26 @@
 <html lang="en">
 <head>
-  <meta charset="utf-8">
+    <meta charset="utf-8">
 
-  <title>Hello World</title>
-  <meta name="Description" content="Hello WOrld">
-  <meta name="Victoria Mears" content="Hello World">
+    <title>Miracle Checkout</title>
+    <meta name="Description" content="Checkout">
+    <meta name="Victoria Mears" content="Checkout">
 
+    <link rel="stylesheet" type="text/css" href="shopping.css">
+    <script src="shopping.js"></script>
 </head>
 
+<?php include 'header.php';?>
 <body>
-  <h1 style="color:red; fontf-family:courier;">Hello World</h1>
+    <form>
+        We deliver by horse and cart. Please Enter Your address.<br><br>
+        Street Address: <input type="text" name="street_address"><br><br>
+        City: <input type="text" name="city">
+        State: <input type="text" name="state"><br><br>
+        Zip Code: <input type="text" name="zip">
+        country: <input type="text" name="country"><br><br>
+        
+    </form>
+  
 </body>
 </html>
