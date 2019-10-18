d705a1cc385d9ef84ff753066616760c3c32e571
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-18 14:26:06 -0600

    change input to clickable div

--- web/rotation_app/create_group.php
+++ web/rotation_app/create_group.php
@@ -18,7 +18,9 @@
     Group Name:<input type="text">
     Comment:<input type="text">
     <div id="couple_fields">
-        <input type="image" name="add_couple_btn" style="width:1em;" src="img/gift_plus.svg"  alt="Plus Icon Designed by Freepik from www.flaticon.com" onclick="addCouple()"> Add Couple
+        <div id="couple_btn" onclick="addCouple()">
+        <img type="image" style="width:1em;" src="img/gift_plus.svg"  alt="Plus Icon Designed by Freepik from www.flaticon.com"> Add Couple
+        </div>
     </div>
     <div id="indiv_fields">
         <input type="image" name="add_indiv_btn" style="width:1em;" src="img/gift_plus.svg"  alt="Plus Icon Designed by Freepik from www.flaticon.com" onclick="addIndiv()"> Add Individual
