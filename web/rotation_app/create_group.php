802c95693acace78331893962569e420505aebe5
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-18 14:38:48 -0600

    add border to add field buttons

--- web/rotation_app/create_group.php
+++ web/rotation_app/create_group.php
@@ -17,13 +17,13 @@
     Password:<input type="text">
     Group Name:<input type="text">
     Comment:<input type="text">
-    <div class="add_field" id="couple_fields">
-        <div id="couple_btn" onclick="addCouple()">
+    <div id="couple_fields">
+        <div class="add_field" id="couple_btn" onclick="addCouple()">
         <img style="width:1em;" src="img/gift_plus.svg"  alt="Plus Icon Designed by Freepik from www.flaticon.com"/> Add Couple
         </div>
     </div>
-    <div class="add_field" id="indiv_fields">
-        <div id="indiv_btn" onclick="addIndiv()">
+    <div id="indiv_fields">
+        <div class="add_field" id="indiv_btn" onclick="addIndiv()">
         <img style="width:1em;" src="img/gift_plus.svg"  alt="Plus Icon Designed by Freepik from www.flaticon.com"/> Add Individual
         </div>
     </div>
