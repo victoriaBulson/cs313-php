2c42b3be074cd4f5f56df02d27aa230b115da63b
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-19 13:34:41 -0600

    testing get

--- web/rotation_app/display_rotation.php
+++ web/rotation_app/display_rotation.php
@@ -2,14 +2,13 @@
 <?php
     echo "php";
     session_start();
-    echo $_SESSION[username];
-    echo $_GET[year];
 
     $statement = $db->prepare
         ("SELECT giver, reciever 
         FROM combos
         WHERE family =". $_GET[username]."
         AND year = ".$_GET[year]);
+    $statement->execute();
 
 ?>
 
