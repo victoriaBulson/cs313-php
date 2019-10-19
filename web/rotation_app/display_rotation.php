455703669e4c76ebaa808a823522938d15a9a02d
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-19 15:53:07 -0600

    added reciever query and CSS

--- web/rotation_app/display_rotation.php
+++ web/rotation_app/display_rotation.php
@@ -35,3 +35,15 @@
     echo '</div>';
 ?>
 
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Chrsitmas Rotation</title>
+    <meta name="Description" content="Gift Rotation">
+    <meta name="Victoria Mears" content="Gift Rotation">
+
+    <link rel="stylesheet" type="text/css" href="rotation.css">
+    <script src="rotation.js"></script>
+</head>
+
