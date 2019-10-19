6109844308dbdddcdaed0fe6d2c440e05bdf4f9c
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-19 12:30:16 -0600

    echo db data

--- web/rotation_app/display_rotaiton.php
+++ web/rotation_app/display_rotaiton.php
@@ -5,5 +5,13 @@
         WHERE family =". $_POST[username]."
         AND year = ".$_POST[year]);
 $statement->execute();
+
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+    $giver = $row['giver']
+	$reciever = $row['reciever'];
+	echo "<p>$giver -> $reciever:<p>";
+}
+
 />
     
