0ecf7b9043b613c82a73921ca82e00a57a38c108
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-19 13:31:59 -0600

    testing get

--- web/rotation_app/display_rotation.php
+++ web/rotation_app/display_rotation.php
@@ -5,5 +5,19 @@
     echo $_SESSION[username];
     echo $_GET[year];
 
+    $statement = $db->prepare
+        ("SELECT giver, reciever 
+        FROM combos
+        WHERE family =". $_GET[username]."
+        AND year = ".$_GET[year]);
+    $statement->execute();
+
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+    $giver = $row['giver']
+	$reciever = $row['reciever'];
+	echo "<p>$giver -> $reciever:<p>";
+}
+
 ?>
 
