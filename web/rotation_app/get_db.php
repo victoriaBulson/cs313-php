b7842e610cc1394b3fa341d84fa3f128846afe5a
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-19 13:49:43 -0600

    testing get

--- web/rotation_app/get_db.php
+++ web/rotation_app/get_db.php
@@ -15,6 +15,8 @@ try
   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    echo 'connection made!';
 }
 catch (PDOException $ex)
 {
