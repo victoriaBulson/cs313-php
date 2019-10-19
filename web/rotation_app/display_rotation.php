3177c86e36e03ff033f4df137589bce2bf30a71d
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-19 15:25:35 -0600

    Try to get name with JOIN

--- web/rotation_app/display_rotation.php
+++ web/rotation_app/display_rotation.php
@@ -5,16 +5,15 @@
     require "get_db.php";
 
     $statement = $db->prepare
-        ("SELECT members.name, combos.giver
-        FROM members
+        ("SELECT combos.giver, members.name
+        FROM combos
         INNER JOIN members ON combos.giver = members.email");
     $statement->execute();
 
     while ($row = $statement->fetch(PDO::FETCH_ASSOC))
     {
         echo 'hello';
-       $name = $row['members.name'];
-        echo '<div>'. $name. '-----------'. $name. '<br>';
+
     }
 ?>
 
