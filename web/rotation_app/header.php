de2fac7f1426d78b57528fb7dd27487655b20f3d
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-19 16:19:38 -0600

    added reciever query and CSS

--- web/rotation_app/header.php
+++ web/rotation_app/header.php
@@ -1,3 +1,4 @@
 <header>
-    <img id="gift_icon" src="img/gift.svg" style="width:3em;" alt="Gift Icon Designed by Pixel perfect from www.flaticon.com">Gift Rotation 
+    <img id="gift_icon" src="img/gift.svg" style="width:1em;" alt="Gift Icon Designed by Pixel perfect from www.flaticon.com">Gift Rotation 
+    <hr style="height:.5em;">
 </header>
