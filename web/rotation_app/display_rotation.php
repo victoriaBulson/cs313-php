2042cd0731367836bcd93c48b6a086da587b390e
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-19 15:50:15 -0600

    added reciever query and CSS

--- web/rotation_app/display_rotation.php
+++ web/rotation_app/display_rotation.php
@@ -10,12 +10,28 @@
         INNER JOIN members ON combos.giver = members.email");
     $statement->execute();
 
+    echo '<div class="list">';
     echo '<div class="givers">';
     while ($row = $statement->fetch(PDO::FETCH_ASSOC))
     {
        $name = $row['name'];
         echo $name. '<br>';
     }
+    echo '</div>';
+
+    $statement = $db->prepare
+        ("SELECT combos.reciever, members.name
+        FROM combos
+        INNER JOIN members ON combos.reciever = members.email");
+    $statement->execute();
+
+    echo '<div class="recievers">';
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+    {
+       $name = $row['name'];
+        echo $name. '<br>';
+    }
+    echo '</div>';
     echo '</div>';
 ?>
 
