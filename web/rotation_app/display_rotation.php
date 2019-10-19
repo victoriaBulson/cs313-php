9585d7f1d78e54f8ef696e1435ed47ace3b0494a
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-19 14:02:17 -0600

    testing get

--- web/rotation_app/display_rotation.php
+++ web/rotation_app/display_rotation.php
@@ -3,8 +3,7 @@
     echo "php wow";
     session_start();
 
-    $statement = $db->prepare
-        (SELECT * FROM accounts);
+    $statement = $db->prepare(SELECT * FROM accounts);
     $statement->execute();
 
     while ($row = $statement->fetch(PDO::FETCH_ASSOC))
