5956345c5a294c3ff5ee32b551c139328a53d938
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-22 15:56:55 -0600

    debug

--- web/w6_team/ta06.php
+++ web/w6_team/ta06.php
@@ -1,4 +1,29 @@
 <?php
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+
 echo '<form action=/ta06b.php method=post>';
 echo 'ID: <input type=text name=id><br>';
 echo 'Book: <input type=text name=book><br>';
@@ -8,7 +33,6 @@ echo 'Content: <input type=textarea name=content><br>';
 
 foreach ($db->query('SELECT name FROM topic') as $row)
 {
-    echo 'HELLO';
 	$topic = $row[name];
   echo '<input type=checkbox name=topic[] value='. $topic .'>' . $topic .'<br>';
 }
--- web/w6_team/ta06b.php
+++ web/w6_team/ta06b.php
@@ -1,4 +1,29 @@
 <?php
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+
 $insert_command = 'INSERT INTO scripture (id, book, chapter, verse, content) VALUES($_POST['id'] , $_POST['book'], $_POST['chapter'], 
 $_POST['verse'], $_POST['content'])';
 $stmt = $this->pdo->prepare($insert_command);
