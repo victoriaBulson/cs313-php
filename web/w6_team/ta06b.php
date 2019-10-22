89ff8a8659978dbd9a8d09ce1f02d7d00eeadf15
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-22 16:27:04 -0600

    debug;

--- web/w6_team/ta06b.php
+++ web/w6_team/ta06b.php
@@ -25,7 +25,7 @@ catch (PDOException $ex)
 
 $insert_scripture = 'INSERT INTO scriptures (id, book, chapter, verse, content) VALUES('. $_POST['id'] .','. '\''. $_POST['book'] . 
 '\''. ',\''. $_POST['chapter'] 
-. '\'' . ',' . '\'' . $_POST['verse'] . '\'' . ',' . '\'' . $_POST['content'] . '\'')';
+. '\'' . ',' . '\'' . $_POST['verse'] . '\'' . ',' . '\'' . $_POST['content'] . '\')';
 $stmt = $this->pdo->prepare($insert_scripture);
 
 
@@ -33,7 +33,7 @@ if(isset($_POST['topic'])) {
     foreach($_POST['topic'] as $topic) {
     		if (isset($topic)) {
     			$topic_id = 'SELECT id FROM topic WHERE name=' . $topic;
-        	$insert_tg = 'INSERT INTO topicalguide (scripture_id) VALUES($_POST['id']);
+        	$insert_tg = 'INSERT INTO topicalguide (scripture_id) VALUES($_POST['id'])';
         	$stmt = $this->pdo->prepare($insert_tg);
         }
     }
