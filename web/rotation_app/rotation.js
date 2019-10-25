f5da0d578832230ad0277f32b0856cb56bf2b4ec
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-24 20:04:32 -0600

    editing create_group.php

--- web/rotation_app/rotation.js
+++ web/rotation_app/rotation.js
@@ -7,8 +7,8 @@ function addCouple(){
     var couple_div = document.createElement('div');
     couple_div.setAttribute('class', 'new_field');
     
-    var form = 'Name 1:<input type="text" name="name1_couple' + num_couples + '">'
-                + 'Name 2:<input type="text" name="name2_couple' + num_couples + '">';
+    var form = 'Name 1:<input type="text" name="name1_couple' + num_couples + '"><br>'
+                + 'Name 2:<input type="text" name="name2_couple' + num_couples + '"><br>';
     
     couple_div.innerHTML = form;
     document.getElementById('couple_fields').appendChild(couple_div);
@@ -20,7 +20,7 @@ function addIndiv(){
     var indiv_div = document.createElement('div');
     indiv_div.setAttribute('class', 'new_field');
     
-    var form = 'Name:<input type="text" name="indiv' + num_indiv + '">';
+    var form = 'Name:<input type="text" name="indiv' + num_indiv + '"><br>';
     
     indiv_div.innerHTML = form;
     document.getElementById('indiv_fields').appendChild(indiv_div);
