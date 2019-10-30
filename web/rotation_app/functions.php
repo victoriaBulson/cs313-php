b29ebd1dfc525167375133ba88029b845e8138ed
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-30 17:44:29 -0600

    executing correct save_account function

--- web/rotation_app/functions.php
+++ web/rotation_app/functions.php
@@ -1,9 +1,9 @@
 <?php
     echo "HEY THERE!";
-    echo $_POST['func'];
-    if($_POST['func'] == 'new_account') {save_account();}
+    echo $_POST['action'];
+    if($_POST['action'] == 'new_account') {save_account();}
 
     function save_account(){
-        echo $_POST['func']; 
+        echo $_POST['action']; 
     }
 ?>
