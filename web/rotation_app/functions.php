8f2a0fa66162273924a33dd1de27027dbb015a4c
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-30 18:11:00 -0600

    Added INSERT statement into save_account()

--- web/rotation_app/functions.php
+++ web/rotation_app/functions.php
@@ -1,12 +1,20 @@
 <?php
-    //require 'get_db.php';
+    require 'get_db.php';
     if($_GET['action'] == 'new_account') {save_account();}
 
     function save_account(){
-        echo $_POST['username'];
-        echo $_POST['password'];
-        echo $_POST['group_name'];
-        echo $_POST['comment'];
+        $username = htmlspecialchars($_POST['username']);
+        $password = htmlspecialchars($_POST['password']);
+        $group_name = htmlspecialchars($_POST['group_name']);
+        $comment = htmlspecialchars($_POST['comment']);
+        
+        $query="INSERT INTO accounts(username, password, name, comment)VALUES(:username, :password, :group_name, :comment)";
+        $stmt=$db->prepare($query);
+        $stmt->bindvalue(':username', $username, PDO::PARAM_STR);
+        $stmt->bindvalue(':password', $password, PDO::PARAM_STR);
+        $stmt->bindvalue(':group_name', $group_name, PDO::PARAM_STR);
+        $stmt->bindvalue(':comment', $comment, PDO::PARAM_STR);
+        $stmt->execute();
     }
 
 ?>
