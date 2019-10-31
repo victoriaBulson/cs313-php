5c14b16ac985433effcfccb39ec4dfa321cdcdc8
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-10-30 18:45:45 -0600

    Added INSERT statement into save_account()

--- web/rotation_app/functions.php
+++ web/rotation_app/functions.php
@@ -3,17 +3,14 @@
     if($_GET['action'] == 'new_account') {save_account();}
 
     function save_account(){
+       /* 
+        //Get Account Info
         $db=get_db();
         $username = htmlspecialchars($_POST['username']);
         $password = htmlspecialchars($_POST['password']);
         $group_name = htmlspecialchars($_POST['group_name']);
         $comment = htmlspecialchars($_POST['comment']);
-        
-        //$stmt=$db->prepare('SELECT username FROM accounts WHERE name="Bulson";');
-        //$stmt->execute();
-        //echo $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-        
+        //Update Account Info;
         $query='INSERT INTO accounts(username, password, name, comment)VALUES(:username, :password, :group_name, :comment);';
         $stmt=$db->prepare($query);
         $stmt->bindvalue(':username', $username, PDO::PARAM_STR);
@@ -21,6 +18,17 @@
         $stmt->bindvalue(':group_name', $group_name, PDO::PARAM_STR);
         $stmt->bindvalue(':comment', $comment, PDO::PARAM_STR);
         $stmt->execute();
+      */  
+
+        foreach($_POST as $key => $value)
+        {
+            if (strstr($key, 'couple'))
+            {
+                echo $key;
+            }
+        }
+
+        
     }
 
 ?>
--- web/rotation_app/rotation.js
+++ web/rotation_app/rotation.js
@@ -7,8 +7,8 @@ function addCouple(){
     var couple_div = document.createElement('div');
     couple_div.setAttribute('class', 'new_field');
     
-    var form = 'Name 1:<input type="text" name="name1_couple' + num_couples + '"><br>'
-                + 'Name 2:<input type="text" name="name2_couple' + num_couples + '"><br>';
+    var form = 'Name 1:<input type="text" name="couple_name1' + num_couples + '"><br>'
+                + 'Name 2:<input type="text" name="couple_name2' + num_couples + '"><br>';
     
     couple_div.innerHTML = form;
     document.getElementById('couple_fields').appendChild(couple_div);
