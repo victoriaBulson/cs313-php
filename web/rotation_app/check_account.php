b3038c5dffa9917dceb076c41cd64b383f668583
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-11-01 16:50:00 -0600

    added password hashing

--- web/rotation_app/check_account.php
+++ web/rotation_app/check_account.php
@@ -6,7 +6,7 @@ $db=get_db();
 $username = htmlspecialchars($_POST['username']);
 $password = htmlspecialchars($_POST['password']);
 
-$query='SELECT password FROM accounts WHERE username=:username;';
+$query='SELECT password FROM accounts;';
 $stmt=$db->prepare($query);
 $stmt->bindvalue(':username', $username, PDO::PARAM_STR);
 $stmt->execute();
