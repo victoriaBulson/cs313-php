f0564bd3715982088ed923399d9f9a4b3f88e509
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-11-01 16:26:28 -0600

    added password hashing

--- web/rotation_app/check_account.php
+++ web/rotation_app/check_account.php
@@ -10,21 +10,9 @@ $query='SELECT * FROM accounts;';
 $stmt=$db->prepare($query);
 //$stmt->bindvalue(':username', $username, PDO::PARAM_STR);
 $stmt->execute();
-$saved_password = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-if (empty($saved_password)){
-    echo 'EMPTY';
-} else{
-    echo '! EMPTY';
-    foreach ($saved_password as $row => $value)
-        echo $row;
-        echo $row[0];
-        echo $value;
-        echo $value[0];
-
-}
-
-
+echo $rows[0][0];
 
 
 ?>
