5eaca5014ad433d2053965c911c2c962fc59e4d2
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-11-02 12:18:54 -0600

    filling in randomized slots

--- web/rotation_app/initialize_list.php
+++ web/rotation_app/initialize_list.php
@@ -11,12 +11,15 @@
         $stmt->bindvalue(':member', $member['email'], PDO::PARAM_STR);
         $stmt->execute();
         $partners = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo $partners[0]['member1'];
         
         /*
         echo $partners[0]['member1']. '<br>', $partners[0]['member2'];
         $slots[$counter] = array($partners[0]['member1'], $partners[0]['member2']);
         
-        if ($member['email'] == $partners[0]['member1'])
+        if ($member['email'] == $partners[0]['member1']){
+            
+        }
         */
     }
     
