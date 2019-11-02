d6d6700c96b9276c3c4ee7ee1ad27c9f0782a9af
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-11-02 14:14:29 -0600

    filling in randomized slots

--- web/rotation_app/initialize_list.php
+++ web/rotation_app/initialize_list.php
@@ -7,7 +7,7 @@
     
     //populate slots with members
     foreach($members as $member){
-        if (!$skip.in_array($member['email'])){
+        if (!in_array($member['email'], $skip)){
             $query='SELECT member1, member2 FROM partners
                 WHERE member1=:member OR member2=:member;';
             $stmt=$db->prepare($query);
