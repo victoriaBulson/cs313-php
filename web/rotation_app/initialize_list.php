3e96190a74bfa535bff9c725d267e1ad479284c6
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-11-02 13:58:22 -0600

    filling in randomized slots

--- web/rotation_app/initialize_list.php
+++ web/rotation_app/initialize_list.php
@@ -2,22 +2,42 @@
     //Vars
     $skip = [];
     $slots = [];
-    $counter = 0;
+    $slots_counter = 0;
+    $skip_counter = 0;
     
+    //populate slots with members
     foreach($members as $member){
-        $query='SELECT member1, member2 FROM partners
+        if (!$skip.includes($member['email'])){
+            $query='SELECT member1, member2 FROM partners
                 WHERE member1=:member OR member2=:member;';
-        $stmt=$db->prepare($query);
-        $stmt->bindvalue(':member', $member['email'], PDO::PARAM_STR);
-        $stmt->execute();
-        $partners = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-        echo $partners[0]['member1']. '<br>', $partners[0]['member2'];
-        /*$slots[$counter] = array($partners[0]['member1'], $partners[0]['member2']);
-        
-        if ($member['email'] == $partners[0]['member1']){
+            $stmt=$db->prepare($query);
+            $stmt->bindvalue(':member', $member['email'], PDO::PARAM_STR);
+            $stmt->execute();
+            $partners = $stmt->fetchAll(PDO::FETCH_ASSOC);
             
-        }*/
+            //if member is part of a couple
+            if (!empty($partners)){
+                echo $partners[0]['member1']. '<br>', $partners[0]['member2'];
+                $slots[$slots_counter] = array($partners[0]['member1'], $partners[0]['member2']);
+                $slots_counter++;
+        
+                if ($member['email'] == $partners[0]['member1']){
+                    $skip[$skip_counter] = $partners[0]['member2']
+                        $skip_counter++;
+                } else{
+                    $skip[$skip_counter] = $partners[0]['member1']
+                        $skip_counter++;
+                }
+            //if member is an individual
+            } else{
+                $slots[$slots_counter] = $member['email'];
+                $slots_counter++;
+            }
+        }
+    }
+
+    foreach ($slots as $slot){
+        echo $slot;
     }
     
 ?>
