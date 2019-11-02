6049ae4bf32e87582826fe57031a2d7c176c2c95
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-11-01 22:31:56 -0600

    Check if year is in database

--- web/rotation_app/display_rotation.php
+++ web/rotation_app/display_rotation.php
@@ -2,7 +2,7 @@
 <?php
     //Get combos from year
     $year = $_GET['year'];
-    query='SELECT year, giver, reciever FROM combos WHERE year=:year;';
+    $query='SELECT giver, reciever FROM combos WHERE year=:year;';
     $stmt=$db->prepare($query);
     $stmt->bindvalue(':year', $year, PDO::PARAM_INT);
     $stmt->execute();
