ad1cd5627e2001a0e9da8dd4761004983e04349a
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-11-02 12:13:59 -0600

    filling in randomized slots

--- web/rotation_app/initialize_list.php
+++ web/rotation_app/initialize_list.php
@@ -5,7 +5,7 @@
     $counter = 0;
     
     foreach( $members as $member){
-        $query='SELECT member1, member2, FROM partners
+        $query='SELECT member1, member2 FROM partners
                 WHERE member1=:member AND member2=:member;';
         $stmt=$db->prepare($query);
         $stmt->bindvalue(':member', $member['email'], PDO::PARAM_STR);
@@ -13,7 +13,8 @@
         $partners = $stmt->fetchAll(PDO::FETCH_ASSOC);
         
         echo $partners[0]['member1']. '<br>', $partners[0]['member2'];
-        //$slots[$counter] = array($partners[0]['member1'], $partners[0]['member2']);
+        $slots[$counter] = array($partners[0]['member1'], $partners[0]['member2']);
+        $
     }
     
 ?>
