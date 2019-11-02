0a9ed59a32ec00a7a4ad54b295c01670e1d0b0fe
Victoria Mears <victoriamears@Victorias-MacBook-Air.local>
2019-11-01 22:52:08 -0600

    display combos

--- web/rotation_app/display_rotation.php
+++ web/rotation_app/display_rotation.php
@@ -1,14 +1,15 @@
 
 <?php
-    echo 'FIRST!';
+    echo 'FIRST!<br>';
     //Get combos from year
     $year = $_GET['year'];
+    echo $year;
     $query='SELECT giver, reciever FROM combos WHERE year=:year;';
     $stmt=$db->prepare($query);
     $stmt->bindvalue(':year', $year, PDO::PARAM_INT);
     $stmt->execute();
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
+    echo $rows[0]['giver'];
     if(empty($rows)){
         //Calculate new combos.
         echo 'empty';
